e48a65551f30c03fcc17192fe6926a4da1af4cb3
shruthiKannappan <cs21b077@smail.iitm.ac.in>
2022-07-09 20:35:38 +0530

    task1

---
+++ form.js
@@ -0,0 +1,10 @@
+function getinput(){
+    const x= document.getElementById('name');
+    console.log(x.value);
+     const y= document.getElementById('college');
+    console.log(y.value);
+     const z= document.getElementById('email');
+    console.log(z.value);
+     const w= document.getElementById('mobile');
+    console.log(w.value);
+  }
